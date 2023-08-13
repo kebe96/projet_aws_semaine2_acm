b53337a3237d86e372edf6b798dc8582736496f8
kebe96 <93620532+kebe96@users.noreply.github.com>
2023-08-13 11:51:54 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import ImageUploader from './ImageUploader';
+import { useState } from 'react';
 
 function App() {
  
  
-    return (
+    // return (
+    //   <div>
+    //     <h1>Page d'accueil</h1>
+    //     <ImageUploader />
+    //   </div>
+    // );
+
+    //import React, { useState } from 'react';
+//import ImageForm from './ImageUploader';
+
+
+  const [images, setImages] = useState([]);
+
+  const addImage = (imageData) => {
+    setImages([...images, imageData]);
+  };
+
+  return (
+    <div>
+      <h1>Page d'accueil</h1>
+      <ImageUploader onSubmit={addImage} />
       <div>
-        <h1>Page d'accueil</h1>
-        <ImageUploader />
+        <h2>Images téléversées :</h2>
+        {images.map((imageData, index) => (
+          <div key={index}>
+            <img src={imageData.image} alt={`Image ${index}`} style={{ maxWidth: '100px' }} />
+            <p>Commentaire : {imageData.comment}</p>
+            <p>Note : {imageData.rating}</p>
+          </div>
+        ))}
       </div>
-    );
-  
-  
-  
+    </div>
+  );
 };
+
 
 export default App;
--- src/ImageUploader.js
+++ src/ImageUploader.js
@@ -1,7 +1,41 @@
 import React, { useState } from 'react';
+import './App.css';
 
-const ImageUploader = () => {
+
+// const ImageUploader = () => {
+//   const [selectedImage, setSelectedImage] = useState(null);
+
+//   const handleImageChange = (event) => {
+//     const file = event.target.files[0];
+//     if (file) {
+//       const reader = new FileReader();
+//       reader.onload = () => {
+//         setSelectedImage(reader.result);
+//       };
+//       reader.readAsDataURL(file);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h2>Uploader une image</h2>
+//       <input type="file" accept="image/*" onChange={handleImageChange} />
+//       {selectedImage && (
+//         <div>
+//           <h3>Image téléversée :</h3>
+//           <img src={selectedImage} alt="Téléversé" style={{ maxWidth: '100%' }} />
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+//import React, { useState } from 'react';
+
+const ImageUploader = ({ onSubmit }) => {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [comment, setComment] = useState('');
+  const [rating, setRating] = useState(0);
 
   const handleImageChange = (event) => {
     const file = event.target.files[0];
@@ -14,18 +48,35 @@ const ImageUploader = () => {
     }
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit({ image: selectedImage, comment, rating });
+    setSelectedImage(null);
+    setComment('');
+    setRating(0);
+  };
+
   return (
-    <div>
-      <h2>Uploader une image</h2>
-      <input type="file" accept="image/*" onChange={handleImageChange} />
-      {selectedImage && (
-        <div>
-          <h3>Image téléversée :</h3>
-          <img src={selectedImage} alt="Téléversé" style={{ maxWidth: '100%' }} />
-        </div>
-      )}
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h2>Ajouter une image</h2>
+      <div>
+        <label>Image :</label>
+        <input type="file" accept="image/*" onChange={handleImageChange} />
+      </div>
+      <div>
+        <label>Commentaire :</label>
+        <textarea value={comment} onChange={(e) => setComment(e.target.value)} />
+      </div>
+      <div>
+        <label>Note :</label>
+        <input type="number" value={rating} onChange={(e) => setRating(e.target.value)} />
+      </div>
+      <button type="submit">Ajouter</button>
+    </form>
   );
 };
+
+//export default ImageForm;
+
 
 export default ImageUploader;
